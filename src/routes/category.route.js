a3e5c1ac807f95b7b254dd373d81689be10f83c7
DanteLuque <danteluque706@gmail.com>
2025-05-02 04:26:42 -0500

    feat(category) - add routes

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -2,10 +2,20 @@
 import express from 'express';
 
 import {
-  getAllCategory
+  getAllCategory,
+  showCreateForm,
+  saveCategory,
+  showEditForm,
+  updateCategory,
+  deleteCategory,
 } from '../controllers/category.controller.js';
 
 const categoryRouter = express.Router();
 categoryRouter.get('/categories', getAllCategory);
+categoryRouter.get('/categories/create', showCreateForm);
+categoryRouter.post('/categories/create', saveCategory);
+categoryRouter.get('/categories/edit/:id', showEditForm);
+categoryRouter.post('/categories/edit/:id', updateCategory)
+categoryRouter.get('/categories/delete/:id', deleteCategory);
 
 export default categoryRouter;
